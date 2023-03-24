c166808c89065ce938f9c2b8b65fe480f2a2a118
lamihck <lamihck@gmail.com>
2023-03-24 17:49:45 +0100

    new file:   craco.config.cjs

--- src/Components/Header/Header.component.js
+++ src/Components/Header/Header.component.js
@@ -5,10 +5,10 @@ export const Header = (props) => {
   return (
     <Fill style={{backgroundColor: '#202123'}}>
       <Left size={'49%'} centerContent={'vertical'}>
-        Left
+        ¤
       </Left>
       <Right size={'49%'} centerContent={'vertical'} style={{textAlign: 'right'}}>
-        Right
+        ¤
       </Right>
     </Fill>
   )
